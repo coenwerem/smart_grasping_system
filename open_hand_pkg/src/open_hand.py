aa7210fbb282b6234f01b216bc714fafd31b6402
coenwerem <clintonenwerem@gmail.com>
2022-01-05 21:01:23 -0500

    Edit open_hand script

--- open_hand_pkg/src/open_hand.py
+++ open_hand_pkg/src/open_hand.py
@@ -9,7 +9,7 @@ point = JointTrajectoryPoint()
 
 rospy.init_node('open_hand_node', anonymous=True)
 pub = rospy.Publisher('/hand_controller/command', JointTrajectory, queue_size=1)
-rate = rospy.Rate(10)
+rate = rospy.Rate(1)
 
 # Message Data
 jointCommands.joint_names = ['H1_F1J1', 'H1_F1J2', 'H1_F1J3', 'H1_F2J1', 'H1_F2J2', 'H1_F2J3', 'H1_F3J1', 'H1_F3J2', 'H1_F3J3']
@@ -32,15 +32,15 @@ jointCommands.points.append(point)
 
 def open_robot_hand():
     while not rospy.is_shutdown():
-        success_info = "Joint commands recorded and published."
-        rospy.loginfo(success_info)
-
         pub.publish(jointCommands)
-        rate.sleep()
+        # rate.sleep()
 
 if __name__ == "__main__":
     try:
+        open_success_info = "Gripper opened!"
+        rospy.loginfo(open_success_info)
         open_robot_hand()
+        rate.sleep()
 
     except rospy.ROSInterruptException:
         pass
